ad6cf9186ce6260502879947474bba684cdb27ba
FulanoD3Tal <su.escala.es.biblica@gmail.com>
2018-06-07 20:55:15 -0500

    Rename and change extension of list of files

--- UltimateFileManager/UltimateFileManagerCore/UltimateFile.cs
+++ UltimateFileManager/UltimateFileManagerCore/UltimateFile.cs
@@ -1,10 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.IO;
 
 namespace UltimateFileManagerCore
 {
     public static class UltimateFile
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="files"></param>
+        /// <param name="newNames"></param>
+        /// <returns></returns>
+        public static List<string> RenameFiles(this IEnumerable<string> files, List<string> newNames)
+        {
+            if (newNames == null)
+            {
+                throw new ArgumentNullException(nameof(newNames));
+            }
+            if (((ICollection<string>)files).Count != newNames.Count)
+            {
+                throw new ArgumentOutOfRangeException(nameof(newNames), "The given names are less that the total of the files");
+            }
+            int i = 0;
+            List<string> newFiles = new List<string>();
+            foreach (string file in files)
+            {
+                newFiles.Add(Path.Combine(Path.GetDirectoryName(file), newNames[i]));
+                i++;
+            }
+            return newFiles;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="files"></param>
+        /// <param name="newExtension"></param>
+        /// <returns></returns>
+        public static List<string> ChangeExtension(this IEnumerable<string> files, string newExtension)
+        {
+            if (string.IsNullOrEmpty(newExtension))
+            {
+                throw new ArgumentNullException(nameof(newExtension));
+            }
+            List<string> newFiles = new List<string>();
+            foreach (string file in files)
+            {
+                newFiles.Add(Path.ChangeExtension(file, newExtension));
+            }
+            return newFiles;
+        }
     }
 }
